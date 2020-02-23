e61aeed632b9c55376e7ccc25b3fcbd021c069fb
Isa Abutaa <isaabutaa@gmail.com>
2020-02-23 17:59:57 -0500

    edabit challenges

--- index.js
+++ index.js
@@ -11,3 +11,27 @@ function getDiscounts(nums, d) {
 	const discount = "." + d.split("").splice(0,2).join("")
 	return nums.map(num => num * Number(discount))
 }
+
+// Create a function that takes two numbers as arguments (num, length) and returns an array of multiples of num up to length.
+function arrayOfMultiples (num, length) {
+	let nums = []
+	for(let i = 1; i <= length; i++) {
+		nums.push(num * i)
+	}
+	return nums
+}
+
+// Write a function that converts an object into an array of keys and values.
+function objectToArray(obj) {
+	let arr = []
+	for(let [key, value] of Object.entries(obj)) {
+		arr.push([key, value])
+	}
+	return arr
+}
+
+// Create a function that concatenates n input arrays, where n is variable.
+function concat(...args) {
+	const arr = []
+	return arr.concat(...args)
+}
